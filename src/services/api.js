bba0778ac7d6ed59329e7c064d25089435f497d7
Pranto-Sen <prantosen56@gmail.com>
2026-07-31 20:13:14 +0600

    fix some issue

--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,15 @@ import axios from "axios";
 import { storage } from "../utils/storage";
 import { refreshToken } from "./authService";
 
+// import axios from "axios";
+
 const api = axios.create({
-  baseURL: "https://localhost:7058/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
+
+// const api = axios.create({
+//   baseURL: "https://localhost:7058/api",
+// });
 
 // api.interceptors.request.use(config => {
 
@@ -32,7 +38,7 @@ api.interceptors.response.use(
 
   async (error) => {
     const originalRequest = error.config;
-     if (
+    if (
       originalRequest.url.includes("/auth/login") ||
       originalRequest.url.includes("/auth/refresh")
     ) {
@@ -46,11 +52,18 @@ api.interceptors.response.use(
         // const tokens = await refreshToken();
         const refreshTokenValue = localStorage.getItem("refreshToken");
 
+        // const response = await axios.post(
+        //   "https://localhost:7058/api/auth/refresh",
+        //   {
+        //     refreshToken: refreshTokenValue,
+        //   },
+        // );
+
         const response = await axios.post(
-          "https://localhost:7058/api/auth/refresh",
+          `${import.meta.env.VITE_API_URL}/auth/refresh`,
           {
             refreshToken: refreshTokenValue,
-          },
+          }
         );
 
         const tokens = response.data;
